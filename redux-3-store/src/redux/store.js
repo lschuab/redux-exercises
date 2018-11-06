576ca8f592c981369046790b78d4c79090725e59
Leandro Schuab <lschuab@gmail.com>
2018-11-06 13:18:25 -0700

    built store

--- redux-3-store/src/redux/store.js
+++ redux-3-store/src/redux/store.js
@@ -1,1 +1,9 @@
 // Create your store here!
+import { createStore, combineReducers} from 'redux'
+import todoReducer from './todos.reducer'
+
+const rootReducer = combineReducer({
+  todos: todoReducer
+})
+
+export default createStore(rootReducer)
